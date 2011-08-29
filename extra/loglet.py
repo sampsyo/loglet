c0c62ac9f53ee502d314707f629ca24352245b31
Adrian Sampson <adrian@radbox.org>
2011-08-29 10:50:32 -0700

    documentation/name changes for async logging

--- extra/loglet.py
+++ extra/loglet.py
@@ -15,9 +15,12 @@ def log(logid, message, level=0):
     }
     urllib.urlopen(url, urllib.urlencode(params))
 
-def threading_apply(object, args=(), kwargs={}):
+
+# Various techniques for running the request asynchronously. 
+
+def threading_apply(func, args=(), kwargs={}):
     """Apply a function in another thread."""
-    t = threading.Thread(target=object, args=args, kwargs=kwargs)
+    t = threading.Thread(target=func, args=args, kwargs=kwargs)
     t.start()
 
 ASYNC_FUNCTIONS['threading'] = threading_apply
@@ -27,9 +30,9 @@ try:
 except ImportError:
     pass
 else:
-    def multiprocessing_apply(object, args=(), kwargs={}):
+    def multiprocessing_apply(func, args=(), kwargs={}):
         """Apply a function in another process."""
-        p = multiprocessing.Process(target=object, args=args, kwargs=kwargs)
+        p = multiprocessing.Process(target=func, args=args, kwargs=kwargs)
         p.start()
     ASYNC_FUNCTIONS['multiprocessing'] = multiprocessing_apply
 
@@ -38,24 +41,33 @@ try:
 except ImportError:
     pass
 else:
-    def gevent_apply(object, args=(), kwargs={}):
+    def gevent_apply(func, args=(), kwargs={}):
         """Apply a function in another green thread."""
-        gevent.spawn(object, *args, **kwargs)
+        gevent.spawn(func, *args, **kwargs)
     ASYNC_FUNCTIONS['gevent'] = gevent_apply
+
+
+# Handler for the Python logging standard library module.
 
 class LogletHandler(logging.Handler):
     """A logging handler that sends messages to a Loglet log. Pass the
     Loglet ID as the first parameter to the constructor.
     """
-    def __init__(self, logid, level=logging.NOTSET, async=None):
+    def __init__(self, logid, level=logging.NOTSET, mode=None):
+        """Create a new Loglet handler. An ID string for the log must
+        be provided. The mode parameter specifies whether log requests
+        should be performed asynchronously. Possible values are
+        "sync" (blocking request), "threading", "multiprocessing", or
+        "gevent".
+        """
         logging.Handler.__init__(self, level)
         self.logid = logid
         try:
-            self.apply = ASYNC_FUNCTIONS[async or 'sync']
+            self.apply = ASYNC_FUNCTIONS[mode or 'sync']
         except KeyError:
             async_types = tuple(ASYNC_FUNCTIONS.iterkeys())
-            raise ValueError(repr(async) + ' is unsupported async type; '
-                             'choose one in ' + repr(async_types))
+            raise ValueError('no such mode ' + repr(mode) + '; ' +
+                             'choose one of ' + repr(async_types))
 
     def emit(self, record):
         args = self.logid, record.getMessage(), record.levelno
